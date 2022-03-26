8c3f2980891a11ac851f89039dacc1904c5e782f
JeancoMontoyaC <92815992+JeanCarloMontoya@users.noreply.github.com>
2022-03-26 18:44:01 -0500

    bjnb

---
+++ src/test/java/test/taller3.televisores/Control.java
@@ -0,0 +1,67 @@
+package taller3.televisores;
+
+
+public class Control {
+	private TV tv;
+	
+	
+	
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		this.tv.control=this;
+	}
+	
+	public TV getTv() {
+		return tv;}
+	public void setTv(TV tv) {
+		this.tv=tv;}
+	
+	public void turnOn() {
+		this.tv.estado=true;}
+	public void turnOff() {
+		this.tv.estado=false;}
+	
+	public void canalUp() {
+		if (tv.estado=true) {
+			if (tv.canal<120) {
+				tv.canal++;}
+			return;}
+		return;}
+	
+	public void canalDown() {
+		if (tv.estado=true) {
+			if (tv.canal>1) {
+				tv.canal--;}
+			return;}
+		return;}
+	
+	public void volumenUp() {
+		if (tv.estado=true) {
+			if (tv.volumen<7) {
+				tv.volumen++;}
+			return;}
+		return;}
+	
+	public void volumenDown() {
+		if (tv.estado=true){
+			if (tv.volumen>0) {
+				tv.volumen--;}
+			return;}
+		return;}
+	
+	public void setCanal(int canal) {
+		if (tv.estado=true) {
+			if (canal>=1||canal<=120) {
+				this.tv.canal=canal;}
+			return;}
+		return;}
+			
+		
+	
+	
+	
+	
+
+}
+	
+	
---
+++ src/test/java/test/taller3.televisores/TV.java
@@ -0,0 +1,56 @@
+package taller3.televisores;
+public class TV {
+	private Marca marca;
+	public int canal;
+	private int precio;
+	public boolean estado;
+	public int volumen;
+	public Control control;
+	private static int numTV=0;
+	
+	public TV(Marca marca,boolean estado) {
+		this.marca=marca;
+		this.estado=estado;
+		canal=1;
+		volumen=1;
+		precio=500;
+	}
+	public void setMarca(Marca marca) {
+		this.marca=marca;}
+	public Marca getMarca() {
+		return marca;}
+	
+	public void setControl(Control control) {
+		this.control=control;}
+	public Control getControl() {
+		return control;}	
+	
+	public void setPrecio(int precio) {
+		this.precio=precio;}
+	public int getPrecio() {
+		return precio;}
+	
+	public void setVolumen(int volumen) {
+		this.volumen=volumen;}
+	public int getVolumen() {
+		return volumen;}
+	
+	public void setCanal(int canal) {
+		this.canal=canal;}
+	public int getCanal() {
+		return canal;}
+	
+	public int getNumTV() {
+		return numTV;}
+	
+	public void turnOn() {
+		estado=true;}
+	public void turnOff() {
+		estado=false;}
+	
+	public boolean getEstado() {
+		return estado;}
+
+	
+
+}
